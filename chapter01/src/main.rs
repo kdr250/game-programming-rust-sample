9ac72fdb53135cba4c4bbdc4e47331de52dffe2f
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-10-11 13:17:26 +0900

    SDL2導入して画面表示できるようにする

--- chapter01/src/main.rs
+++ chapter01/src/main.rs
@@ -1,3 +1,39 @@
+use std::time::Duration;
+
+use sdl2::{event::Event, keyboard::Keycode, pixels::Color, rect::Rect};
+
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl2::init().unwrap();
+    let video_system = sdl_context.video().unwrap();
+
+    let window = video_system
+        .window("Rust-SDL2", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+
+        canvas.set_draw_color(Color::RGB(0, 0, 255));
+        canvas.clear();
+
+        canvas.set_draw_color(Color::RGB(255, 255, 255));
+        canvas.fill_rect(Rect::new(200, 200, 100, 100)).unwrap();
+
+        canvas.present();
+        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
 }
