bf6fc853690e69b6c8ad712ddb191b08c2b2c663
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-10-11 16:35:59 +0900

    1.4 ゲームループとゲームクラス

---
+++ chapter01/src/game.rs
@@ -0,0 +1,71 @@
+use anyhow::{anyhow, Result};
+use sdl2::{
+    event::Event,
+    keyboard::{KeyboardState, Scancode},
+    video::Window,
+    EventPump, Sdl,
+};
+
+pub struct Game {
+    context: Sdl,
+    window: Window,
+    event_pump: EventPump,
+    is_running: bool,
+}
+
+impl Game {
+    /// Initialize game
+    pub fn initialize() -> Result<Game> {
+        let context = sdl2::init().map_err(|e| anyhow!(e))?;
+
+        let video_system = context.video().map_err(|e| anyhow!(e))?;
+
+        let window = video_system
+            .window("Game Programming in Rust", 1024, 768)
+            .position(100, 100)
+            .build()?;
+
+        let event_pump = context.event_pump().map_err(|e| anyhow!(e))?;
+
+        Ok(Game {
+            context,
+            window,
+            event_pump,
+            is_running: true,
+        })
+    }
+
+    /// Runs the game loop until the game is over
+    pub fn run_loop(&mut self) {
+        while self.is_running {
+            self.process_input();
+            self.update_game();
+            self.generate_output();
+        }
+    }
+
+    /// Shutdown the game
+    pub fn shutdown(&mut self) {}
+
+    /// Herlper functions for the game loop
+    fn process_input(&mut self) {
+        for event in self.event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } => {
+                    self.is_running = false;
+                    break;
+                }
+                _ => {}
+            }
+        }
+
+        let state = KeyboardState::new(&self.event_pump);
+        if state.is_scancode_pressed(Scancode::Escape) {
+            self.is_running = false;
+        }
+    }
+
+    fn update_game(&mut self) {}
+
+    fn generate_output(&mut self) {}
+}
--- chapter01/src/main.rs
+++ chapter01/src/main.rs
@@ -1,39 +1,9 @@
-use std::time::Duration;
+mod game;
 
-use sdl2::{event::Event, keyboard::Keycode, pixels::Color, rect::Rect};
+use game::Game;
 
 fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_system = sdl_context.video().unwrap();
-
-    let window = video_system
-        .window("Rust-SDL2", 800, 600)
-        .position_centered()
-        .build()
-        .unwrap();
-
-    let mut canvas = window.into_canvas().build().unwrap();
-    let mut event_pump = sdl_context.event_pump().unwrap();
-
-    'running: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                _ => {}
-            }
-        }
-
-        canvas.set_draw_color(Color::RGB(0, 0, 255));
-        canvas.clear();
-
-        canvas.set_draw_color(Color::RGB(255, 255, 255));
-        canvas.fill_rect(Rect::new(200, 200, 100, 100)).unwrap();
-
-        canvas.present();
-        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    if let Ok(mut game) = Game::initialize() {
+        game.run_loop();
     }
 }
