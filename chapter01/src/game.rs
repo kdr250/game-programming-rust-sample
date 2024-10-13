1fd2128a55430bece37138630d77bbc80a8fc734
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-10-13 11:41:02 +0900

    1.5.4 壁とボールとパドルの描画

--- chapter01/src/game.rs
+++ chapter01/src/game.rs
@@ -3,16 +3,26 @@ use sdl2::{
     event::Event,
     keyboard::{KeyboardState, Scancode},
     pixels::Color,
+    rect::Rect,
     render::Canvas,
     video::Window,
     EventPump, Sdl,
 };
+
+const THICKNESS: u32 = 15;
+
+struct Vector2 {
+    x: f32,
+    y: f32,
+}
 
 pub struct Game {
     context: Sdl,
     canvas: Canvas<Window>,
     event_pump: EventPump,
     is_running: bool,
+    paddle_position: Vector2,
+    ball_position: Vector2,
 }
 
 impl Game {
@@ -31,11 +41,23 @@ impl Game {
 
         let event_pump = context.event_pump().map_err(|e| anyhow!(e))?;
 
+        let paddle_position = Vector2 {
+            x: 10.0,
+            y: 768.0 / 2.0,
+        };
+
+        let ball_position = Vector2 {
+            x: 1024.0 / 2.0,
+            y: 768.0 / 2.0,
+        };
+
         Ok(Game {
             context,
             canvas,
             event_pump,
             is_running: true,
+            paddle_position,
+            ball_position,
         })
     }
 
@@ -74,6 +96,20 @@ impl Game {
     fn generate_output(&mut self) {
         self.canvas.set_draw_color(Color::RGBA(0, 0, 255, 255));
         self.canvas.clear();
+
+        self.canvas.set_draw_color(Color::RGBA(255, 255, 255, 255));
+
+        let wall = Rect::new(0, 0, 1024, THICKNESS);
+        self.canvas.fill_rect(wall).unwrap();
+
+        let ball = Rect::new(
+            self.ball_position.x as i32 - THICKNESS as i32 / 2,
+            self.ball_position.y as i32 - THICKNESS as i32 / 2,
+            THICKNESS,
+            THICKNESS,
+        );
+        self.canvas.fill_rect(ball).unwrap();
+
         self.canvas.present();
     }
 }
