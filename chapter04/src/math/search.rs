b3a2b8d6d06f6e9eeb0160816ac91ced05bbdc5f
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-10-23 22:09:05 +0900

    4.2.4 欲張り最良優先探索(GBFS)

--- chapter04/src/math/search.rs
+++ chapter04/src/math/search.rs
@@ -40,18 +40,48 @@ pub struct Graph {
     nodes: Vec<Rc<RefCell<GraphNode>>>,
 }
 
+static WEIGHTED_ID: AtomicU32 = AtomicU32::new(0);
+
+pub fn generate_weighted_id() -> u32 {
+    let id = WEIGHTED_ID.load(Ordering::SeqCst);
+    WEIGHTED_ID.fetch_add(1, Ordering::SeqCst);
+    id
+}
+
+#[derive(Debug, PartialEq)]
 pub struct WeightedEdge {
-    from: Rc<WeightedGraphNode>,
-    to: Rc<WeightedGraphNode>,
+    from: Rc<RefCell<WeightedGraphNode>>,
+    to: Rc<RefCell<WeightedGraphNode>>,
     weight: f32,
 }
 
+impl WeightedEdge {
+    pub fn new(
+        from: Rc<RefCell<WeightedGraphNode>>,
+        to: Rc<RefCell<WeightedGraphNode>>,
+        weight: f32,
+    ) -> Self {
+        Self { from, to, weight }
+    }
+}
+
+#[derive(Debug, PartialEq)]
 pub struct WeightedGraphNode {
-    edges: Vec<WeightedEdge>,
+    id: u32,
+    edges: Vec<Rc<RefCell<WeightedEdge>>>,
+}
+
+impl WeightedGraphNode {
+    pub fn new() -> Self {
+        Self {
+            id: generate_weighted_id(),
+            edges: vec![],
+        }
+    }
 }
 
 pub struct WeightedGraph {
-    nodes: Vec<Rc<WeightedGraphNode>>,
+    nodes: Vec<Rc<RefCell<WeightedGraphNode>>>,
 }
 
 type NodeToParentMap = HashMap<u32, Rc<RefCell<GraphNode>>>;
@@ -86,11 +116,121 @@ pub fn bfs(
     path_found
 }
 
+#[derive(Debug)]
+pub struct GBFSScratch {
+    parent_edge: Option<Rc<RefCell<WeightedEdge>>>,
+    heuristic: f32,
+    in_open_set: bool,
+    in_closed_set: bool,
+}
+
+impl GBFSScratch {
+    pub fn new() -> Self {
+        Self {
+            parent_edge: None,
+            heuristic: 0.0,
+            in_open_set: false,
+            in_closed_set: false,
+        }
+    }
+}
+
+type GBFSMap = HashMap<u32, Rc<RefCell<GBFSScratch>>>;
+
+fn compute_heuristic(
+    _a: &Rc<RefCell<WeightedGraphNode>>,
+    _b: &Rc<RefCell<WeightedGraphNode>>,
+) -> f32 {
+    0.0
+}
+
+pub fn gbfs(
+    start: Rc<RefCell<WeightedGraphNode>>,
+    goal: Rc<RefCell<WeightedGraphNode>>,
+    out_map: &mut GBFSMap,
+) -> bool {
+    let mut open_set = vec![];
+
+    let mut current = start;
+    let mut scratch = GBFSScratch::new();
+    scratch.in_closed_set = true;
+    let scratch_ref = Rc::new(RefCell::new(scratch));
+    out_map.insert(current.borrow().id, scratch_ref);
+
+    let mut is_first = true;
+    while is_first || current.borrow().id != goal.borrow().id {
+        for edge in current.borrow().edges.clone() {
+            let id = edge.borrow().to.borrow().id;
+            let data = if let Some(scratch) = out_map.get(&id) {
+                scratch.clone()
+            } else {
+                let scratch = Rc::new(RefCell::new(GBFSScratch::new()));
+                out_map.insert(id, scratch.clone());
+                scratch
+            };
+            let mut borrowed_data = data.borrow_mut();
+            if !borrowed_data.in_closed_set {
+                borrowed_data.parent_edge = Some(edge.clone());
+                if !borrowed_data.in_open_set {
+                    borrowed_data.heuristic = compute_heuristic(&edge.borrow().to, &goal);
+                    borrowed_data.in_open_set = true;
+                    open_set.push(edge.borrow().to.clone());
+                }
+            }
+        }
+
+        if open_set.is_empty() {
+            break;
+        }
+
+        let cloned_open_set = open_set.clone();
+        let result = cloned_open_set
+            .into_iter()
+            .min_by(|a, b| {
+                let aa = out_map.get(&a.borrow().id);
+                let a_heuristic = if let Some(scratch) = aa {
+                    scratch.borrow().heuristic
+                } else {
+                    0.0
+                };
+                let bb = out_map.get(&b.borrow().id);
+                let b_heuristic = if let Some(scratch) = bb {
+                    scratch.borrow().heuristic
+                } else {
+                    0.0
+                };
+                a_heuristic.partial_cmp(&b_heuristic).unwrap()
+            })
+            .unwrap();
+
+        current = result.clone();
+        open_set.retain(|node| node.borrow().id != result.borrow().id);
+        let current_id = current.borrow().id;
+        let update_scratch = if let Some(scratch) = out_map.get(&current_id) {
+            scratch.clone()
+        } else {
+            let scratch = Rc::new(RefCell::new(GBFSScratch::new()));
+            out_map.insert(current_id, scratch.clone());
+            scratch
+        };
+        update_scratch.borrow_mut().in_open_set = false;
+        update_scratch.borrow_mut().in_closed_set = true;
+        is_first = false;
+    }
+
+    let found = current.borrow().id == goal.borrow().id;
+
+    found
+}
+
 #[cfg(test)]
 mod tests {
     use std::{cell::RefCell, rc::Rc};
 
-    use super::{bfs, Graph, GraphNode, NodeToParentMap};
+    use super::{
+        bfs, gbfs, GBFSMap, Graph, GraphNode, NodeToParentMap, WeightedEdge, WeightedGraph,
+        WeightedGraphNode,
+    };
 
     #[test]
     fn test_bfs() {
@@ -146,5 +286,52 @@ mod tests {
         let actual = answers.into_iter().rev().collect::<Vec<String>>().join("");
 
         assert_eq!("[0,0][1,0][1,1][1,2][1,3][1,4]", actual);
+    }
+
+    #[test]
+    fn test_gbfs() {
+        let mut g = WeightedGraph { nodes: vec![] };
+
+        for _ in 0..5 {
+            for _ in 0..5 {
+                let node = Rc::new(RefCell::new(WeightedGraphNode::new()));
+                g.nodes.push(node);
+            }
+        }
+
+        for i in 0..5_usize {
+            for j in 0..5_usize {
+                let node = g.nodes[i * 5 + j].clone();
+                if i > 0 {
+                    let from = node.clone();
+                    let to = g.nodes[(i - 1) * 5 + j].clone();
+                    let e = WeightedEdge::new(from, to, 1.0);
+                    node.borrow_mut().edges.push(Rc::new(RefCell::new(e)));
+                }
+                if i < 4 {
+                    let from = node.clone();
+                    let to = g.nodes[(i + 1) * 5 + j].clone();
+                    let e = WeightedEdge::new(from, to, 1.0);
+                    node.borrow_mut().edges.push(Rc::new(RefCell::new(e)));
+                }
+                if j > 0 {
+                    let from = node.clone();
+                    let to = g.nodes[i * 5 + j - 1].clone();
+                    let e = WeightedEdge::new(from, to, 1.0);
+                    node.borrow_mut().edges.push(Rc::new(RefCell::new(e)));
+                }
+                if j < 4 {
+                    let from = node.clone();
+                    let to = g.nodes[i * 5 + j + 1].clone();
+                    let e = WeightedEdge::new(from, to, 1.0);
+                    node.borrow_mut().edges.push(Rc::new(RefCell::new(e)));
+                }
+            }
+        }
+
+        let mut map = GBFSMap::new();
+        let found = gbfs(g.nodes[0].clone(), g.nodes[9].clone(), &mut map);
+
+        assert!(found, "GBFS not found...");
     }
 }
