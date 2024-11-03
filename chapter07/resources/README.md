bf2f3a513409671d31aec3d32892bdf4d0348c75
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-11-03 13:01:05 +0900

    7.1.4 バンクとイベント

--- chapter07/resources/README.md
+++ chapter07/resources/README.md
@@ -1,2 +1,2 @@
 ## アセットについて
-[サンプルレポジトリのAssets](https://github.com/gameprogcpp/code/tree/master/Chapter06/Assets)ディレクトリをダウンロードしてここに配置すること。
+[サンプルレポジトリのAssets](https://github.com/gameprogcpp/code/tree/master/Chapter07/Assets)ディレクトリをダウンロードしてここに配置すること。
--- chapter07/src/system/audio_system.rs
+++ chapter07/src/system/audio_system.rs
@@ -1,9 +1,9 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{cell::RefCell, collections::HashMap, path::Path, rc::Rc};
 
 use anyhow::Result;
 use libfmod::{
     ffi::{FMOD_INIT_NORMAL, FMOD_STUDIO_INIT_NORMAL},
-    Studio, System,
+    Bank, EventDescription, LoadBank, Studio, System,
 };
 
 use super::asset_manager::AssetManager;
@@ -12,6 +12,8 @@ pub struct AudioSystem {
     asset_manager: Rc<RefCell<AssetManager>>,
     system: Studio,
     low_level_system: System,
+    banks: HashMap<String, Bank>,
+    events: HashMap<String, EventDescription>,
 }
 
 impl AudioSystem {
@@ -23,13 +25,58 @@ impl AudioSystem {
 
         let low_level_system = system.get_core_system()?;
 
-        let this = Self {
+        let mut this = Self {
             asset_manager,
             system,
             low_level_system,
+            banks: HashMap::new(),
+            events: HashMap::new(),
         };
 
+        this.load_bank("Master Bank.strings.bank")?;
+        this.load_bank("Master Bank.bank")?;
+
         Ok(Rc::new(RefCell::new(this)))
+    }
+
+    pub fn load_bank(&mut self, name: &str) -> Result<()> {
+        // Prevent double-loading
+        if self.banks.contains_key(name) {
+            return Ok(());
+        }
+
+        // load bank
+        let path = Path::new(env!("OUT_DIR"))
+            .join("resources")
+            .join("Assets")
+            .join(name);
+        let name = path.to_str().unwrap();
+
+        let bank = self.system.load_bank_file(name, LoadBank::NORMAL)?;
+        self.banks.insert(name.to_string(), bank);
+        bank.load_sample_data()?;
+
+        let num_events = bank.get_event_count()?;
+        if num_events <= 0 {
+            return Ok(());
+        }
+
+        let events = bank.get_event_list(num_events)?;
+        for event in events {
+            let event_name = event.get_path()?;
+            self.events.insert(event_name, event);
+        }
+
+        Ok(())
+    }
+
+    pub fn play_event(&mut self, name: &str) {
+        if let Some(event_description) = self.events.get(name) {
+            if let Ok(event_instance) = event_description.create_instance() {
+                let _ = event_instance.start();
+                let _ = event_instance.release();
+            }
+        }
     }
 
     pub fn update(&mut self, _delta_time: f32) {
