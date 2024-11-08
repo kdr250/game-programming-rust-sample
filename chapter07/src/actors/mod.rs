a1652f536a933bbb8e62a22c80dd7bf290cbd6af
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-11-08 16:40:49 +0900

    余計なファイル削除

--- chapter07/src/actors/mod.rs
+++ chapter07/src/actors/mod.rs
@@ -1,4 +1,3 @@
 pub mod actor;
-pub mod asteroid;
 pub mod camera_actor;
 pub mod plane_actor;
--- chapter09/src/actors/mod.rs
+++ chapter09/src/actors/mod.rs
@@ -1,5 +1,4 @@
 pub mod actor;
-pub mod asteroid;
 pub mod camera_actor;
 pub mod follow_actor;
 pub mod fps_actor;
