3bde64e6e151a7560b2b4a24ad97f6f901972061
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-11-13 10:24:51 +0900

    10.2.1 点の包含判定- カプセルの包含判定

--- chapter10/src/collision/capsule.rs
+++ chapter10/src/collision/capsule.rs
@@ -18,4 +18,32 @@ impl Capsule {
     pub fn point_on_segment(&self, t: f32) -> Vector3 {
         self.segment.point_on_segment(t)
     }
+
+    pub fn contains(&self, point: &Vector3) -> bool {
+        let dist_sq = self.segment.min_dist_sq(point);
+        dist_sq <= self.radius * self.radius
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::math::vector3::Vector3;
+
+    use super::Capsule;
+
+    #[test]
+    fn test_contains() {
+        let capsule = Capsule::new(Vector3::ZERO, Vector3::new(0.0, 5.0, 0.0), 1.0);
+        let actual = capsule.contains(&Vector3::new(0.8, 3.0, 0.0));
+
+        assert!(actual);
+    }
+
+    #[test]
+    fn test_not_contains() {
+        let capsule = Capsule::new(Vector3::ZERO, Vector3::new(0.0, 5.0, 0.0), 1.0);
+        let actual = capsule.contains(&Vector3::new(1.8, 3.0, 0.0));
+
+        assert!(!actual);
+    }
 }
