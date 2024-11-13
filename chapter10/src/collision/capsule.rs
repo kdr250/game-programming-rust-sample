0606f1be45196bd15a864ab212149f657c99eaeb
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-11-13 11:39:11 +0900

    10.2.2 バウンディングボリューム間の交差判定 - カプセルとカプセルの判定

--- chapter10/src/collision/capsule.rs
+++ chapter10/src/collision/capsule.rs
@@ -23,6 +23,12 @@ impl Capsule {
         let dist_sq = self.segment.min_dist_sq(point);
         dist_sq <= self.radius * self.radius
     }
+
+    pub fn intersect(&self, other: &Capsule) -> bool {
+        let line_dist_sq = LineSegment::min_dist_sq_line(&self.segment, &other.segment);
+        let sum_radius = self.radius + other.radius;
+        line_dist_sq <= sum_radius * sum_radius
+    }
 }
 
 #[cfg(test)]
@@ -43,6 +49,32 @@ mod tests {
     fn test_not_contains() {
         let capsule = Capsule::new(Vector3::ZERO, Vector3::new(0.0, 5.0, 0.0), 1.0);
         let actual = capsule.contains(&Vector3::new(1.8, 3.0, 0.0));
+
+        assert!(!actual);
+    }
+
+    #[test]
+    fn test_intersect() {
+        let a = Capsule::new(Vector3::ZERO, Vector3::new(0.0, 5.0, 0.0), 1.0);
+        let b = Capsule::new(
+            Vector3::new(1.0, 0.0, 0.0),
+            Vector3::new(1.0, 5.0, 0.0),
+            1.0,
+        );
+        let actual = Capsule::intersect(&a, &b);
+
+        assert!(actual);
+    }
+
+    #[test]
+    fn test_not_intersect() {
+        let a = Capsule::new(Vector3::ZERO, Vector3::new(0.0, 5.0, 0.0), 1.0);
+        let b = Capsule::new(
+            Vector3::new(3.0, 0.0, 0.0),
+            Vector3::new(3.0, 5.0, 0.0),
+            1.0,
+        );
+        let actual = Capsule::intersect(&a, &b);
 
         assert!(!actual);
     }
--- chapter10/src/collision/line_segment.rs
+++ chapter10/src/collision/line_segment.rs
@@ -1,4 +1,4 @@
-use crate::math::vector3::Vector3;
+use crate::math::{self, vector3::Vector3};
 
 pub struct LineSegment {
     start: Vector3,
@@ -35,6 +35,95 @@ impl LineSegment {
         let scaler = Vector3::dot(&ac, &ab) / Vector3::dot(&ab, &ab);
         let p = ab * scaler;
         (ac - p).length_sq()
+    }
+
+    pub fn min_dist_sq_line(&self, other: &LineSegment) -> f32 {
+        let u = self.end.clone() - other.start.clone();
+        let v = other.end.clone() - self.start.clone();
+        let w = self.start.clone() - other.start.clone();
+
+        let a = Vector3::dot(&u, &u); // always >= 0
+        let b = Vector3::dot(&u, &v);
+        let c = Vector3::dot(&v, &v); // always >= 0
+        let d = Vector3::dot(&u, &w);
+        let e = Vector3::dot(&v, &w);
+
+        let discriminant = a * c - b * b; // always >= 0
+        let mut sc = discriminant; // sc = sN / sD, default sD = D >= 0
+        let mut sn = discriminant;
+        let mut sd = discriminant;
+        let mut tc = discriminant; // tc = tN / tD, default tD = D >= 0
+        let mut tn = discriminant;
+        let mut td = discriminant;
+
+        // compute the line parameters of the two closest points
+        if math::basic::near_zero(discriminant, 0.001) {
+            // the lines are almost parallel
+            // force using point P0 on segment S1
+            // to prevent possible division by 0.0 later
+            sn = 0.0;
+            sd = 1.0;
+            tn = e;
+            td = c;
+        } else {
+            // get the closest points on the infinite lines
+            sn = b * e - c * d;
+            tn = a * e - b * d;
+            if sn < 0.0 {
+                // sc < 0 => the s=0 edge is visible
+                sn = 0.0;
+                tn = e;
+                td = c;
+            } else if sn > sd {
+                // sc > 1  => the s=1 edge is visible
+                sn = sd;
+                tn = e + b;
+                td = c;
+            }
+        }
+
+        if tn < 0.0 {
+            // tc < 0 => the t=0 edge is visible
+            tn = 0.0;
+            // recompute sc for this edge
+            if -d < 0.0 {
+                sn = 0.0;
+            } else if -d > a {
+                sn = sd;
+            } else {
+                sn = -d;
+                sd = a;
+            }
+        } else if tn > td {
+            // tc > 1  => the t=1 edge is visible
+            tn = td;
+            // recompute sc for this edge
+            if (-d + b) < 0.0 {
+                sn = 0.0;
+            } else if (-d + b) > a {
+                sn = sd;
+            } else {
+                sn = -d + b;
+                sd = a;
+            }
+        }
+
+        // finally do the division to get sc and tc
+        sc = if math::basic::near_zero(sn, 0.001) {
+            0.0
+        } else {
+            sn / sd
+        };
+        tc = if math::basic::near_zero(tn, 0.001) {
+            0.0
+        } else {
+            tn / td
+        };
+
+        // get the difference of the two closest points
+        let dp = w + (u * sc) - (v * tc);
+
+        dp.length_sq()
     }
 }
 
