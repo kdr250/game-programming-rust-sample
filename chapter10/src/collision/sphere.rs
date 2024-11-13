e753c2fb9297e1dceb1ea1f83475dcac95bce9d4
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-11-13 10:02:10 +0900

    10.2.1 点の包含判定- 球の包含判定

--- chapter10/src/collision/sphere.rs
+++ chapter10/src/collision/sphere.rs
@@ -9,4 +9,32 @@ impl Sphere {
     pub fn new(center: Vector3, radius: f32) -> Self {
         Self { center, radius }
     }
+
+    pub fn contains(&self, point: Vector3) -> bool {
+        let dist_sq = (self.center.clone() - point).length_sq();
+        dist_sq <= self.radius * self.radius
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::math::vector3::Vector3;
+
+    use super::Sphere;
+
+    #[test]
+    fn test_contains() {
+        let sphere = Sphere::new(Vector3::ZERO, 2.0);
+        let actual = sphere.contains(Vector3::new(0.8, 0.8, 0.8));
+
+        assert!(actual);
+    }
+
+    #[test]
+    fn test_not_contains() {
+        let sphere = Sphere::new(Vector3::ZERO, 1.0);
+        let actual = sphere.contains(Vector3::new(0.8, 0.8, 0.8));
+
+        assert!(!actual);
+    }
 }
